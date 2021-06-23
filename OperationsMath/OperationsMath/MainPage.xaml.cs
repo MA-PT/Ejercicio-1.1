054f45fe6501309819adafbb5c59f422f4da97d3
MA-PT <pineda.marvin173@gmail.com>
2021-06-22 19:20:09 -0600

    Ejercicio 1.1 Uploaded

---
+++ OperationsMath/OperationsMath/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OperationsMath
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Sumar_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(num1.Text) && !String.IsNullOrWhiteSpace(num2.Text))
+            {
+                int n1 = Convert.ToInt32(num1.Text);
+                int n2 = Convert.ToInt32(num2.Text);
+                int result = n1 + n2;
+                await Navigation.PushModalAsync(new Segunda(result));
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Llene Ambos Campos para Continuar", "Cerrar");
+            }
+        }
+
+        private async void Dividir_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(num1.Text) && !String.IsNullOrWhiteSpace(num2.Text))
+            {
+                int n1 = Convert.ToInt32(num1.Text);
+                int n2 = Convert.ToInt32(num2.Text);
+                int result = n1 / n2;
+                await Navigation.PushModalAsync(new Segunda(result));
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Llene Ambos Campos para Continuar", "Cerrar");
+            }
+        }
+
+        private async void Multiplicar_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(num1.Text) && !String.IsNullOrWhiteSpace(num2.Text))
+            {
+
+                int n1 = Convert.ToInt32(num1.Text);
+                int n2 = Convert.ToInt32(num2.Text);
+                int result = n1 * n2;
+                await Navigation.PushModalAsync(new Segunda(result));
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Llene Ambos Campos para Continuar", "Cerrar");
+            }
+        }
+
+        private async void Restar_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(num1.Text) && !String.IsNullOrWhiteSpace(num2.Text))
+            {
+                int n1 = Convert.ToInt32(num1.Text);
+                int n2 = Convert.ToInt32(num2.Text);
+                int result = n1 - n2;
+                await Navigation.PushModalAsync(new Segunda(result));
+            }
+            else
+            {
+                await DisplayAlert ("Advertencia", "Llene Ambos Campos para Continuar", "Cerrar");
+            }
+        }
+    }
+}
---
+++ OperationsMath/OperationsMath/Segunda.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OperationsMath
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Segunda : ContentPage
+    {
+        public Segunda(int result)
+        {
+            InitializeComponent();
+
+            Result.Text = Convert.ToString(result);
+        }
+    }
+}
